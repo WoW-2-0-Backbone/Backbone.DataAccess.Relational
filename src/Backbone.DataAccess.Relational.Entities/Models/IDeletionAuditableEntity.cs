61247869f37b24bc063db2d7049e1a62c1777d50
Sultonbek Rakhimov <sultonbek.rakhimov@gmail.com>
2024-09-09 18:57:55 +0500

    Added entity abstraction interfaces

---
+++ src/Backbone.DataAccess.Relational.Entities/Models/IDeletionAuditableEntity.cs
@@ -0,0 +1,12 @@
+﻿namespace Backbone.DataAccess.Relational.Entities.Models;
+
+/// <summary>
+/// Defines an entity that is auditable for deletion tracking.
+/// </summary>
+public interface IDeletionAuditableEntity
+{
+    /// <summary>
+    /// Gets or sets the ID of the user who deleted the entity.
+    /// </summary>
+    Guid? DeletedByUserId { get; set; }
+}
---
+++ src/Backbone.DataAccess.Relational.Entities/Models/ISoftDeletedEntity.cs
@@ -0,0 +1,17 @@
+﻿namespace Backbone.DataAccess.Relational.Entities.Models;
+
+/// <summary>
+/// Defines an entity that supports soft deletion.
+/// </summary>
+public interface ISoftDeletedEntity
+{
+    /// <summary>
+    /// Gets or sets entity deleted indicator.
+    /// </summary>
+    public bool IsDeleted { get; set; }
+
+    /// <summary>
+    /// Gets or sets entity deleted time.
+    /// </summary>
+    public DateTimeOffset? DeletedTime { get; set; }
+}
